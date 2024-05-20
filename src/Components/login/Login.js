c37b526aa196ff3813c94c74b876738aecf35234
Suryaprkash03 <suryatheking5784@gmail.com>
2024-05-20 21:26:09 +0530

    Login Done

--- src/Components/login/Login.js
+++ src/Components/login/Login.js
@@ -1,52 +1,63 @@
 import { useState } from 'react';
-import './Login.css';
 import { useNavigate } from 'react-router-dom';
 import style from './Login.module.css';
-import DataContext from '../../context/DataContext';
 
-const Login = () => {
-    const { setLogin } = useContext(DataContext);
-    const navigate = useNavigate();
+
+
+const Login = ({ setLogin }) => {
+    const navigate = useNavigate()
     const [input, setInput] = useState({ email: '', password: '' });
-    const [errorMessage, setErrorMessage] = useState('');
-
+    const [errorMessage, seterrorMessage] = useState('');
     const handleChange = e => {
         setInput({ ...input, [e.target.name]: e.target.value });
     };
-
     const formSubmitter = e => {
         e.preventDefault();
-        if (!input.email) return setErrorMessage('Please enter a valid email ID');
+        if (!input.email) return seterrorMessage('Please enter valid email id');
         if (!input.password)
-            return setErrorMessage(
-                'Password should have a minimum of 8 characters with a combination of uppercase, lowercase, numbers, and special characters'
+            return seterrorMessage(
+                'Password should have minimum 8 character with the combination of uppercase, lowercase, numbers and specialcharaters'
             );
-        if (input.email !== 'admin' || input.password !== 'saravanan') return setErrorMessage('Invalid email or password');
+        if (input.email !== 'admin' || input.password !== 'admin') return seterrorMessage('Invalid email or password')
         else {
-            navigate('/Home');
-            setLogin(false);
+            navigate('/Home')
+            setLogin(false)
         }
+        ;
     };
 
+
     return (
-        <div className='background-image'>
-            <div class="login-box">
+        <div className={style.background_image}>
+            <div className={style.login_box}>
                 <div>
-                    <img src="./assets/Logo.jpg" alt="" />
+                    <img src="./assets/Logo.jpg" alt="" className={style.img} />
                 </div>
                 <form onSubmit={formSubmitter}>
                     {errorMessage.length > 0 && <div style={{ marginBottom: '10px', color: 'red', marginLeft: '30px' }}>{errorMessage}</div>}
-                    <div class="user-box">
-                        <input type="text" name="email" required="" onChange={handleChange} placeholder='Username' />
+                    <div class={style.user_box}>
+                        <input
+                            type="text"
+                            name="email"
+                            required=""
+                            onChange={handleChange}
+                            placeholder='Username'
+                            className={style.input} />
                     </div>
-                    <div class="user-box">
-                        <input type="password" name="password" required="" onChange={handleChange} placeholder='Password' />
-                    </div>
-                    <div>
-                        <button type="submit">Login</button>
+                    <div class={style.user_box}>
+                        <input
+                            type="password"
+                            name="password"
+                            required=""
+                            onChange={handleChange}
+                            placeholder='Password'
+                            className={style.input} />
                     </div>
                     <div >
-                        <a href="#" className='a'>Forgot password?</a>
+                        <button>Login</button>
+                    </div>
+                    <div >
+                        <a href="#" className={style.a}>Forgot password?</a>
                     </div>
                 </form>
             </div>
