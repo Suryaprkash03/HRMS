1b5bcc5062b88fb17a5865309092d5b3f18982f0
Mohammad shihabudeen <city.shihabudeen@gmail.com>
2024-05-20 21:39:31 +0530

    login

--- src/Components/login/Login.js
+++ src/Components/login/Login.js
@@ -1,29 +1,30 @@
-import { useState } from 'react';
+import { useState ,useContext} from 'react';
 import { useNavigate } from 'react-router-dom';
 import style from './Login.module.css';
+import DataContext from '../../context/DataContext';
 
 
 
-const Login = ({ setLogin }) => {
+const Login = () => {
+    const { setLogin } = useContext(DataContext);
     const navigate = useNavigate()
     const [input, setInput] = useState({ email: '', password: '' });
-    const [errorMessage, seterrorMessage] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
     const handleChange = e => {
         setInput({ ...input, [e.target.name]: e.target.value });
     };
     const formSubmitter = e => {
         e.preventDefault();
-        if (!input.email) return seterrorMessage('Please enter valid email id');
+        if (!input.email) return setErrorMessage('Please enter a valid email ID');
         if (!input.password)
-            return seterrorMessage(
-                'Password should have minimum 8 character with the combination of uppercase, lowercase, numbers and specialcharaters'
+            return setErrorMessage(
+                'Password should have a minimum of 8 characters with a combination of uppercase, lowercase, numbers, and special characters'
             );
-        if (input.email !== 'admin' || input.password !== 'admin') return seterrorMessage('Invalid email or password')
+        if (input.email !== 'admin' || input.password !== 'saravanan') return setErrorMessage('Invalid email or password');
         else {
-            navigate('/Home')
-            setLogin(false)
+            navigate('/Home');
+            setLogin(false);
         }
-        ;
     };
 
 
